e9813322ba9e88ded0df9710b172d2695faf922e
James Neely <jimboneely@Jamess-MacBook-Pro.local>
2018-01-19 18:38:28 -0500

    With button

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -9,9 +9,9 @@ const MainPage = () =>
 <h1 className="mb-1">Madpass</h1>
 </div>
 <h3 className="mb-5">
-  <em>Security is a Laughing Matter. </em>
+  <em>Security is no Laughing Matter. </em>
 </h3>
-<Link to="/questiongen">Generate Passphrase</Link>
+<Link to="/questiongen"><button className="btn-main">Generate Passphrase</button></Link>
 </div>
 </div>;
 
