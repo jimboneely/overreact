1b9e896ec61c3bf5530b7904b101c8beae975158
Rolando Cintron <rolando.cintron@gmail.com>
2018-01-19 18:18:57 -0500

    created imagedisplayjsclass

--- client/src/components/ImageDisplay/ImageDisplay.js
+++ client/src/components/ImageDisplay/ImageDisplay.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+class App extends Component {
+    state = {
+        pagenumber:""
+    }
+    //Image JS Code
+
+
+    //end Image JS Code
+
+    render() {
+      return (
+        <h2>THe image HTML goes here</h2>
+      );
+    }
+  }
