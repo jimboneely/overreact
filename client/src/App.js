e36b490e6494edea903c6c6d0733dd8743176b89
James Neely <jimboneely@Jamess-MacBook-Pro.local>
2018-01-19 15:03:53 -0500

    QuestionGen React Route need link

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import QuestionGen from "./components/QuestionGen";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import './App.css';
@@ -16,6 +17,7 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <MainPage />
+        <Route exact path="/questiongen" component={QuestionGen} />
           <Switch>
 
           </Switch>
--- client/src/components/QuestionGen/QuestionGen.js
+++ client/src/components/QuestionGen/QuestionGen.js
@@ -1,2 +1,8 @@
 import React from 'react';
 
+const QuestionGen = () =>
+  <div>
+    <h1> ask user For person, place, thing, and number </h1>
+  </div>
+
+export default QuestionGen;
