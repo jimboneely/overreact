7e021e54952b070bf10dde874a9c3cc9fc20b8ef
James Neely <jimboneely@Jamess-MacBook-Pro.local>
2018-01-23 20:33:29 -0500

    Added Form with its own Component Css

--- client/src/components/QuestionGen/QuestionGen.js
+++ client/src/components/QuestionGen/QuestionGen.js
@@ -1,8 +1,53 @@
 import React from 'react';
-
-const QuestionGen = () =>
-  <div className="wordform">
-    <h1> ask user For person, place, thing, and number </h1>
-  </div>;
-
+import "./QuestionGen.css";
+const QuestionGen = props => (
+<div className="question-body">
+<form>
+  <div className="form-group">
+    <h4>
+      <strong>Noun</strong>
+    </h4>
+    <input
+      className="form-control"
+      type="text"
+      value={props.noun}
+      name="noun"
+      onChange={props.handleInputChange}
+      required
+    />
+    <h4>
+      <strong>Verb</strong>
+    </h4>
+    <input
+      className="form-control"
+      type="text"
+      value={props.verb}
+      name="verb"
+      onChange={props.handleInputChange}
+      required
+    />
+    <h4>
+      <strong>Number</strong>
+    </h4>
+    <input
+      className="form-control"
+      type="number"
+      value={props.number}
+      name="number"
+      onChange={props.handleInputChange}
+      required
+    />
+  </div>
+  <div className="pull-right">
+    <button
+      onClick={props.handleFormSubmit}
+      type="submit"
+      className="btn btn-lg"
+    >
+      Submit
+    </button>
+  </div>
+</form>
+</div>
+);
 export default QuestionGen;
