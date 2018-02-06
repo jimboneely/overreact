40eb5f557313ed6283ea95312dc0aa049bb1e105
Rolando Cintron <rolando.cintron@gmail.com>
2018-02-06 00:51:15 -0500

    Fixes to MainPage

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -27,7 +27,7 @@ const MainPage = () =>
 <svg width="211px" height="221px" viewBox="0 0 211 221" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
     <desc>Created with Sketch.</desc>
     <defs></defs>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-weight="normal" font-family="Flavors-Regular, Flavors" letter-spacing="40" font-size="288">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontWeight="normal" fontFamily="Flavors-Regular, Flavors" letterSpacing="40" fontSize="288">
         <g id="Group" transform="translate(0.000000, -77.000000)" fill="#3AAFA9">
             <text id="M">
                 <tspan x="0" y="296">M</tspan>
@@ -40,7 +40,7 @@ const MainPage = () =>
 <svg width="225px" height="225px" viewBox="0 0 225 225" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
     <desc>Created with Sketch.</desc>
     <defs></defs>
-    <g id="Page-1" class="a-letter-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-weight="normal" font-family="Flavors-Regular, Flavors" letter-spacing="40" font-size="288">
+    <g id="Page-1" className="a-letter-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontWeight="normal" fontFamily="Flavors-Regular, Flavors" letterSpacing="40" fontSize="288">
         <g id="Group"  fill="#2B7A78">
             <text id="a">
                 <tspan x="40" y="210">a</tspan>
@@ -53,7 +53,7 @@ const MainPage = () =>
 <svg width="225px" height="222px" viewBox="0 0 225 222" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
     <desc>Created with Sketch.</desc>
     <defs></defs>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-weight="normal" font-family="Flavors-Regular, Flavors" letter-spacing="40" font-size="288">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontWeight="normal" fontFamily="Flavors-Regular, Flavors" letterSpacing="40" fontSize="288">
         <g id="Group" transform="translate(-465.000000, -72.000000)" fill="#3AAFA9">
             <text id="d">
                 <tspan x="461" y="285">d</tspan>
@@ -66,7 +66,7 @@ const MainPage = () =>
 <svg width="225px" height="223px" viewBox="0 0 225 223" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
     <desc>Created with Sketch.</desc>
     <defs></defs>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-weight="normal" font-family="Flavors-Regular, Flavors" letter-spacing="40" font-size="288">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontWeight="normal" fontFamily="Flavors-Regular, Flavors" letterSpacing="40" fontSize="288">
         <g id="Group" transform="translate(-761.000000, -77.000000)" fill="#2B7A78">
             <text id="P">
                 <tspan x="800" y="296">P</tspan>
@@ -79,7 +79,7 @@ const MainPage = () =>
 <svg width="225px" height="225px" viewBox="0 0 225 225" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
     <desc>Created with Sketch.</desc>
     <defs></defs>
-    <g id="Page-1" class="a-letter-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-weight="normal" font-family="Flavors-Regular, Flavors" letter-spacing="40" font-size="288">
+    <g id="Page-1" className="a-letter-2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontWeight="normal" fontFamily="Flavors-Regular, Flavors" letterSpacing="40" fontSize="288">
         <g id="Group"  fill="#3AAFA9">
             <text id="a" >
                 <tspan x="40" y="210">a</tspan>
@@ -93,7 +93,7 @@ const MainPage = () =>
 <svg width="259px" height="280px" viewBox="0 0 259 280" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
     <desc>Created with Sketch.</desc>
     <defs></defs>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-size="288" font-family="Flavors-Regular, Flavors" letter-spacing="40" font-weight="normal">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontSize="288" fontFamily="Flavors-Regular, Flavors" letterSpacing="40" fontWeight="normal">
         <text id="ss">
             <tspan x="-7" y="300" fill="#2B7A78">s</tspan>
             <tspan x="143.53125" y="350" fill="#3AAFA9">s</tspan>
